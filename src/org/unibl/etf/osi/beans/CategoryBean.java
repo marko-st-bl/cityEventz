0aef6493774882d207680dd140f3d5f9302b7476
Marko Stojanovic <st.marko.bl@gmail.com>
2019-12-14 13:13:13 +0100

    Modifed Category DAO and Bean

--- src/org/unibl/etf/osi/beans/CategoryBean.java
+++ src/org/unibl/etf/osi/beans/CategoryBean.java
@@ -1,5 +1,32 @@
 package org.unibl.etf.osi.beans;
 
+import java.util.List;
+
+import org.unibl.etf.osi.dao.CategoryDAO;
+import org.unibl.etf.osi.dto.Category;
+
 public class CategoryBean {
+	
+	private Category category=new Category();
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	public List<Category> getAllCategories(){
+		return CategoryDAO.getAllCategories();
+	}
+	
+	public void addCategory(String name) {
+		CategoryDAO.addCategory(name);
+	}
+	
+	public void removeCategory(String name) {
+		CategoryDAO.removeCategory(name);
+	}
 
 }
--- src/org/unibl/etf/osi/dao/CategoryDAO.java
+++ src/org/unibl/etf/osi/dao/CategoryDAO.java
@@ -1,8 +1,11 @@
 package org.unibl.etf.osi.dao;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.unibl.etf.osi.dto.Category;
@@ -10,52 +13,63 @@ import org.unibl.etf.osi.dto.Category;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
 
 public class CategoryDAO {
 	
 	public static final String PATH="C:\\Users\\Marko\\eclipse-workspace\\cityEventz\\json\\categories.json";
 	
-	public static List<Category> getAllCategories() throws Exception{
-		List<Category> retVal;
+	public static List<Category> getAllCategories(){
+		List<Category> retVal=new ArrayList<>();
 		
 		Type CATEGORY_TYPE = new TypeToken<List<Category>>() {
 		}.getType();
 		Gson gson=new Gson();
-		JsonReader reader = new JsonReader(new FileReader(PATH));
-		retVal=gson.fromJson(reader, CATEGORY_TYPE);
-		reader.close();
-		
+		JsonReader reader;
+		try {
+			reader = new JsonReader(new FileReader(PATH));
+			retVal=gson.fromJson(reader, CATEGORY_TYPE);
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return retVal;
 	}
 	
-	public static void addCategory(String name) throws Exception {
-		List<Category> categories=getAllCategories();
-		Category category=new Category(categories.size(), name);
-		categories.add(category);
-		Gson gson = new Gson();
-		FileWriter writer = new FileWriter(PATH);
-		gson.toJson(categories,writer);
-		writer.close();
-	}
-	
-	public static void removeCategory(String name) throws Exception{
-		List<Category> categories=getAllCategories();
-		Category cat=new Category();
-		for(Category c:categories) {
-			if(c.getName().equals(name)) {
-				cat=c;
-			}
+	public static void addCategory(String name) {
+		List<Category> categories;
+		try {
+			categories = getAllCategories();
+			Category category=new Category(categories.size(), name);
+			categories.add(category);
+			Gson gson = new Gson();
+			FileWriter writer = new FileWriter(PATH);
+			gson.toJson(categories,writer);
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		categories.remove(cat);
-		Gson gson = new Gson();
-		FileWriter writer = new FileWriter(PATH);
-		gson.toJson(categories, writer);
-		writer.close();
 	}
 	
-	public static void main(String[] args) throws Exception {
-		removeCategory("Game");
+	public static void removeCategory(String name){
+		List<Category> categories;
+		try {
+			categories = getAllCategories();
+			Category cat=new Category();
+			for(Category c:categories) {
+				if(c.getName().equals(name)) {
+					cat=c;
+				}
+			}
+			categories.remove(cat);
+			Gson gson = new Gson();
+			FileWriter writer = new FileWriter(PATH);
+			gson.toJson(categories, writer);
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 }
